1d49682994f42bb8b19d58e5e02d39509aae0b2d
Darius Kazemi <darius.kazemi@gmail.com>
2013-09-05 15:28:09 -0400

    hashtag

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function generate(nounPlural, verb) {
   nounPlural = nounPlural || nouns.pick().pluralize();
   verb = verb || nouns.pick().pluralize();
   var generatedText = '<span class="word">&nbsp;' + verb + '&nbsp;</span> are the jQuery of <span class="word">&nbsp;' + nounPlural + '&nbsp;</span>';
-  var shareText = verb + ' are the jQuery of ' + nounPlural;
+  var shareText = verb + ' are the jQuery of ' + nounPlural + ' #jqProclamations';
   $('#content').html(generatedText);
   var shareUrl = window.location.href.split('?')[0]+'?word='+sharing.encodeStr(verb)+'$'+sharing.encodeStr(nounPlural);
   $('#share').attr('href', shareUrl);
